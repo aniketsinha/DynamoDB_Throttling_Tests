dd20ed6cf66666f5ea8bc55646b41976f158b2f6
Aniket Sinha <aniketsinha@users.noreply.github.com>
2017-02-22 18:22:54 +0530

    Multiple test at short intervals
    Short bursts at 5 seconds interval

--- src/com/aniket/Main.java
+++ src/com/aniket/Main.java
@@ -8,13 +8,24 @@ import com.amazonaws.auth.AWSCredentials;
 public class Main {
 
     public static void main(String[] args) {
-	    Stats stats = new Stats();
-        int numberOfThreads = 1200;
+	Stats stats = new Stats();
+        int numberOfThreads = 100;
         AWSCredentials awsCredentials = TestsUtil.getAwsCredentials();
-        for(int i=0; i<numberOfThreads;i++) {
-            Thread readThread = new Thread(new ThrottleRead(awsCredentials,stats));
-            readThread.start();
-        }
-
+	try {
+		System.out.println("Initiating test...");
+		for(int j=1;j<=15;j++) {
+			System.out.println(" o_o Thread awake...");			
+			for(int i=0; i<numberOfThreads;i++) {
+			    Thread readThread = new Thread(new ThrottleRead(awsCredentials,stats));
+			    readThread.start();
+			}
+			System.out.println(" -_- Thread going to sleep...");
+		        Thread.sleep(5000);
+		}
+		System.out.println("Test complete!");
+	}
+	catch(Exception e) {
+	    System.out.println("Exception occured, e="+e);
+	}
     }
 }
